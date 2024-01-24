d8d1bad76af948b11baca58af1b3b22d013caf02
dawidsadowski <dawidsadowski01@gmail.com>
2024-01-23 17:48:15 -0800

    Dzien 4 praca domowa

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var age = 28;
+var sex = "female";
+var name = "Ewa";
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("kobieta ponizej 30lat");
+}
+
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+
+
